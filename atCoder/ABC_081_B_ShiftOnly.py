589fd6046f054e7b98737c41b260bd5793c55d9b
nyankiti <nyancic77@outlook.jp>
2021-02-03 22:12:35 +0900

    ShiftOnly問題recursionを用いた回答作成

--- atCoder/ABC_081_B_ShiftOnly.py
+++ atCoder/ABC_081_B_ShiftOnly.py
@@ -1,6 +1,6 @@
 '''
 【問題概要】
-黒板に N 個の正の整数 A1,…,A_N が書かれています。
+黒板に N 個の正の整数 A_1,…,A_N が書かれています。
 すぬけ君は，黒板に書かれている整数がすべて偶数であるとき，次の操作を行うことができます。
 
 黒板に書かれている整数すべてを，2 で割ったものに置き換える。
@@ -25,11 +25,36 @@ for 文を用いた全探索
 '''
 
 
-def main2021_02_02(a, b):
+def main2021_02_03_recursion(N, numbers):
+  def _main(N, numbers):
+    for i in range(N):
+      if numbers[i]%2 == 1:
+        return numbers
+    numbers = list(map(lambda x: x//2, numbers))
+    sub_result = _main(N, numbers)
+    return sub_result
+  result = _main(N, numbers)
+  return result
 
+def main2021_02_03(N, numbers):
+  flg = True
+  while (flg):
+    for i in range(N):
+      if numbers[i]%2 == 1:
+        flg = False
+        break
+    if flg:
+      numbers = list(map(lambda x: x//2, numbers))
+  return numbers
+
+
+def main(N, numbers):
+  for i in range(N):
+    if numbers[i]%2 == 1:
+      return numbers
 
 
 #  if __name__ == '__main__': の分の中の処理は python3 sorts.py とコマンドで呼び出した時に自動的に呼ばれる
 if __name__ == '__main__':
-  s = main2021_02_02(3, 4)
+  s = main2021_02_02_recursion(3, [32, 64, 24])
   print(s)
