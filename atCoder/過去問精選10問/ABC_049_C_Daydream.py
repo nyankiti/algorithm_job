f262f532449552f0ec4504c5d731726b767e9d60
nyankiti <nyancic77@outlook.jp>
2021-02-26 10:01:35 +0900

    update

--- atCoder/過去問精選10問/ABC_049_C_Daydream.py
+++ atCoder/過去問精選10問/ABC_049_C_Daydream.py
@@ -21,13 +21,60 @@ Greedy アルゴリズム
 端から決まって行く Greedy アルゴリズム
 後ろから解く
 '''
+import random
+
+def main2021_02_08(S):
+  T = ''
+  lists = ["dream", "dreamer", "erase", "eraser"]
+  while len(S) > len(T):
+    num = random.randint(0, 3)
+    T = T + lists[num]
+    if S == T:
+      return 'YES'
+  return 'NO'
+
+def main2021_02_09(S):
+  T = ''
+  lists = ["dream", "dreamer", "erase", "eraser"]
+  candidate = []
+
+  for i in range(4):
+    T = T + lists[i]
+    candidate.append(T)
+    for j in range(4):
+      candidate.append(T + lists[j])
+      print(candidate)
+    T = ''
+  if S in candidate:
+    return 'YES'
+  print("dream" in candidate)
+  return 'NO'
 
 
-def main2021_02_02(a, b):
+def main2021_02_14(query):
+  while 1:
+    for flag in ["dream", "dreamer", "erase", "eraser"]:
+      if query.endswith(flag):
+        # スライスを用いて後ろから削っていくことがポイント
+        query = query[:-len(flag)]
+        break
+      else:
+        print("NO")
+        break
+      
+      if not query:
+        print('YES')
+        break
 
 
+
+
+def main2021_02_10(S):
+  T = ''
+  lists = ["dream", "dreamer", "erase", "eraser"]
+  candidate = []
 
 #  if __name__ == '__main__': の分の中の処理は python3 sorts.py とコマンドで呼び出した時に自動的に呼ばれる
 if __name__ == '__main__':
-  s = main2021_02_02(3, 4)
+  s = main2021_02_09("dreameraser")
   print(s)
