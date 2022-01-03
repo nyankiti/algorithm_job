e418f204da938b26ce4445cb2c25f266ffddd76f
nyankiti <nyancic77@outlook.jp>
2022-01-03 12:39:05 +0900

    50pp 1~5

--- 50popularProblems/4FindTheDuplicate.py
+++ 50popularProblems/4FindTheDuplicate.py
@@ -1,19 +1,37 @@
 '''
-
+1~nの数がn+1の長さの配列に格納された入力を受け取る。ただし、重複する数は一つとする
+pigeonhole principleより、必ず一つの数は重複していることがわかる。
+Floyd's cycle detection algorithmを用いることができる重要な問題
 '''
 import random
 
+
 def main(arr):
-  visited = {}
-  for element in arr:
-    if visited.get(element):
-      return element
-    else:
-      visited[element] = True
-  return 'There was no duplicates'
+    visited = {}
+    for element in arr:
+        if visited.get(element):
+            return element
+        else:
+            visited[element] = True
+    return 'There was no duplicates'
+
+
+def findDuplicateTortoiseHare(arr):
+    tortoise = arr[0]
+    hare = arr[arr[0]]
+    while tortoise != hare:
+        tortoise = arr[tortoise]
+        hare = arr[arr[hare]]
+    tortoise = 0
+    while tortoise != hare:
+        tortoise = arr[tortoise]
+        hare = arr[hare]
+    return tortoise
 
 
 #  if __name__ == '__main__': の分の中の処理は python3 sorts.py とコマンドで呼び出した時に自動的に呼ばれる
 if __name__ == '__main__':
-  s = main([1, 4, 2, 2, 5, 2])
-  print(s)
+    s = main([1, 4, 2, 2, 5, 2])
+    print(s)
+    ss = findDuplicateTortoiseHare([1, 4, 2, 2, 5, 2])
+    print(ss)
--- 50popularProblems/5ThreeDepthFirstSearch.py
+++ 50popularProblems/5ThreeDepthFirstSearch.py
@@ -4,173 +4,76 @@ dfs : depth first search
 import random
 
 
-# define binary tree
-# class Node(object):
-#     def __init__(self, value: int):
-#         self.value = value
-#         self.left = None
-#         self.right = None
-
-# class BinarySearchTree(object):
-#   def __init__(self):
-#     self.root = None
-  
-#   def insert(self, value):
-#     if self.root is None:
-#       self.root = Node(value)
-      
-
-#     def _insert(node, value):
-#       if node is None:
-#         return Node(value)
-
-#       if value < node.value:
-#         node.left = _insert(node.left, value)
-#       else:
-#         node.right = _insert(node.right, value)
-#       return node
-    
-#     _insert(self.root, value)
-
-
-
-class Node(object):
-    def __init__(self, value: int) -> None:
+class Node:
+    def __init__(self, value, left=None, right=None):
         self.value = value
         self.left = None
         self.right = None
 
 
-class BinarySearchTree(object):
-
-    def __init__(self) -> None:
+class Tree:
+    def __init__(self):
         self.root = None
 
-    def insert(self, value: int) -> None:
+    def insert(self, value):
         if self.root is None:
             self.root = Node(value)
             return
 
-        def _insert(node: Node, value: int) -> Node:
+        def _insert(node, value):
             if node is None:
                 return Node(value)
 
-            if value < node.value:
-                node.left = _insert(node.left, value)
-            else:
+            if node.value < value:
                 node.right = _insert(node.right, value)
+            else:
+                node.left = _insert(node.left, value)
             return node
 
         _insert(self.root, value)
 
-    def inorder(self) -> None:
-        def _inorder(node: Node) -> None:
-            if node is not None:
-                _inorder(node.left)
-                print(node.value)
-                _inorder(node.right)
-        _inorder(self.root)
-
-
-    def search(self, value: int) -> bool:
-        def _search(node: Node, value: int) -> bool:
-            if node is None:
-                return False
-
-            if node.value == value:
-                return True
-            elif node.value > value:
-                return _search(node.left, value)
-            elif node.value < value:
-                return _search(node.right, value)
-        return _search(self.root, value)
-
-    def min_value(self, node: Node) -> Node:
-        current = node
-        while current.left is not None:
-            current = current.left
-        return current
-
-    def remove(self, value: int) -> None:
-        def _remove(node: Node, value: int) -> Node:
-            if node is None:
-                return node
-
-            if value < node.value:
-                node.left = _remove(node.left, value)
-            elif value > node.value:
-                node.right = _remove(node.right, value)
-            else:
-                if node.left is None:
-                    return node.right
-                elif node.right is None:
-                    return node.left
-
-                temp = self.min_value(node.right)
-                node.value = temp.value
-                node.right = _remove(node.right, temp.value)
-            return node
-        _remove(self.root, value)
-
-    def dfsPreorder(self)->None:
-        def _dfsPreorder(root)->None:
-          if root is None:
-            return
-          print(root.value)
-          _dfsPreorder(root.left)
-          _dfsPreorder(root.right)
+    def dfsPreorder(self):
+        def _dfsPreorder(root):
+            if root is None:
+                return
+            print(root.value)
+            _dfsPreorder(root.left)
+            _dfsPreorder(root.right)
 
         _dfsPreorder(self.root)
 
-    def dfsPostorder(self)->None:
-        def _dfsPostorder(root)->None:
-          if root is None:
-            return
-          _dfsPostorder(root.left)
-          _dfsPostorder(root.right)
-          print(root.value)
-
-        _dfsPostorder(self.root)
-
-    def dfsInorder(self)->None:
-        def _dfsInorder(root)->None:
-          if root is None:
-            return
-          _dfsInorder(root.left)
-          print(root.value)
-          _dfsInorder(root.right)
+    def dfsInorder(self):
+        def _dfsInorder(root):
+            if root is None:
+                return
+            _dfsInorder(root.left)
+            print(root.value)
+            _dfsInorder(root.right)
 
         _dfsInorder(self.root)
 
+    def dfsPostorder(self):
+        def _dfsPostorder(root):
+            if root is None:
+                return
+            _dfsPostorder(root.left)
+            _dfsPostorder(root.right)
+            print(root.value)
+        _dfsPostorder(self.root)
 
 
-
-    # def dfsInorder(root):
-    #   if root is None:
-    #     return
-    #   dfsInorder(root.left)
-    #   print(root.value)
-    #   dfsInorder(root.right)
-
-    # def dfsPostorder(root):
-    #   if root is None:
-    #     return
-    #   dfsPostorder(root.left)
-    #   dfsPostorder(root.right)
-    #   print(root.value)
-
-
-
-#  if __name__ == '__main__': の分の中の処理は python3 sorts.py とコマンドで呼び出した時に自動的に呼ばれる
 if __name__ == '__main__':
-  binary_tree = BinarySearchTree()
-  binary_tree.insert(1)
-  binary_tree.insert(2)
-  binary_tree.insert(3)
-  binary_tree.insert(4)
-  binary_tree.insert(5)
-  binary_tree.insert(6)
-
-  # binary_tree.dfsPreorder()
-  # binary_tree.dfsPostorder()
-  binary_tree.dfsInorder()
+    tree = Tree()
+    tree.insert(1)
+    tree.insert(2)
+    tree.insert(3)
+    tree.insert(44)
+    tree.insert(5)
+    tree.insert(6)
+    tree.insert(7)
+    print("inorder--------------------------------------")
+    tree.dfsInorder()
+    print("postorder------------------------------------")
+    tree.dfsPostorder()
+    print("preorder-------------------------------------")
+    tree.dfsPreorder()
