fe2fbffd0f5304a5a534f9cf99801694382f00f1
nyankiti <nyancic77@outlook.jp>
2022-01-07 12:36:21 +0900

    build adCoder env

---
+++ atCoder/runtime/example/command.sh
@@ -0,0 +1,11 @@
+# 以下のコマンドを実行すると、testできる
+# ~/src/algorithm_job/atCoder/runtime/example/command.sh
+
+echo test1 result below-----------------------------------------------------------------
+echo;
+python3 ~/src/algorithm_job/atCoder/runtime/example/answer.py < ~/src/algorithm_job/atCoder/runtime/example/input1.txt
+echo;
+echo;
+echo test2 result below-----------------------------------------------------------------
+echo;
+python3 ~/src/algorithm_job/atCoder/runtime/example/answer.py < ~/src/algorithm_job/atCoder/runtime/example/input2.txt
