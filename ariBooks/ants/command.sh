8602e8aecc5e24c0d39691da70af3509680cb73b
nyankiti <nyancic77@outlook.jp>
2022-01-22 11:12:01 +0900

    build ariBooks env

---
+++ ariBooks/ants/command.sh
@@ -0,0 +1,11 @@
+# 以下のコマンドを実行すると、testできる
+# ~/src/algorithm_job/ariBooks/ants/command.sh
+
+echo test1 result below-----------------------------------------------------------------
+echo;
+python3 ~/src/algorithm_job/ariBooks/ants/answer.py < ~/src/algorithm_job/ariBooks/ants/input1.txt
+echo;
+echo;
+echo test2 result below-----------------------------------------------------------------
+echo;
+python3 ~/src/algorithm_job/ariBooks/ants/answer.py < ~/src/algorithm_job/ariBooks/ants/input2.txt
