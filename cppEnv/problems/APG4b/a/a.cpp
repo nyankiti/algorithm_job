c9dad696a6895646952fb255ef6a27e2befa4fd5
nyankiti <nyancic77@outlook.jp>
2022-05-17 15:40:58 +0900

    APG a~i

---
+++ cppEnv/problems/APG4b/a/a.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  // endl は改行を表す。
+  cout << "Hello, world!" << endl;
+  return 0;
+}
---
+++ cppEnv/problems/APG4b/b_出力とコメント/b.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  // endl は改行を表す。
+  cout << "こんにちは" << endl;
+  cout << "AtCoder" << endl;
+  return 0;
+}
