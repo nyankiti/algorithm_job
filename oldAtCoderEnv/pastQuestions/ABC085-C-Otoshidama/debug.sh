e5e7d17016827bade905fee1db8e47de26584bd2
nyankiti <nyancic77@outlook.jp>
2022-07-25 21:09:39 +0900

    refactor folder structure

--- oldAtCoderEnv/pastQuestions/ABC085-C-Otoshidama/debug.sh
+++
@@ -1,4 +0,0 @@
-# 以下のコマンドを実行すると、debug testできる
-# ~/src/algorithm_job/atCoder/pastQuestions/ABC085-C-Otoshidama/debug.sh
-
-/usr/bin/env /usr/bin/python3 /Users/sodakentaro/.vscode/extensions/ms-python.python-2021.10.1365161279/pythonFiles/lib/python/debugpy/launcher 56558 -- /Users/sodakentaro/src/algorithm_job/atCoder/pastQuestions/ABC085-C-Otoshidama/answer.py ~/src/algorithm_job/atCoder/pastQuestions/ABC085-C-Otoshidama/input1.txt
