b22e740d4ad4f6bcd980284a8f6f8c65712bc79a
nyankiti <nyancic77@outlook.jp>
2023-03-21 14:18:30 +0900

    update template

--- atCoderEnv/acc-template-cpp/main.cpp
+++ atCoderEnv/acc-template-cpp/main.cpp
@@ -10,7 +10,12 @@ using vvi = vector<vi>; // 二次元vector
 using vs = vector<string>;
 using pii = pair<int, int>;
 /* macro */
-#define rep(i, a, n) for (int i = (a); i < (n); ++i)
+#define MOD 1000000007
+#define MOD2 998244353
+#define INF ((1 << 30) - 1)
+#define LINF (1LL << 60)
+// 非常に小さい値を表す epsilon。浮動小数展比較において、誤差の範囲を表すために使用する。10^(-10)を表している。
+#define EPS (1e-10)
 
 
 int main() {
